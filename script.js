f675665193b1248bb28618fc4a043425b08e9762
queryquestschool <134953472+queryquestschool@users.noreply.github.com>
2023-05-31 12:53:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const courses = document.querySelectorAll('.course');
-  
-    courses.forEach(function(course) {
-      course.addEventListener('click', function() {
-        this.classList.toggle('active');
-      });
+  const courses = document.querySelectorAll('.course');
+
+  courses.forEach(function(course) {
+    const description = course.querySelector('p'); // Get the course description element
+
+    course.addEventListener('click', function() {
+      this.classList.toggle('active');
+      description.style.display = this.classList.contains('active') ? 'block' : 'none';
     });
   });
-  
+});
+
